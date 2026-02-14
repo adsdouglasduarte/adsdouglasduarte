c929cdda09ed81b631bd16a78f1ff146f248c6a8
Douglas Duarte <adsdouglasduarte@gmail.com>
2026-02-14 14:41:34 -0300

    FIX: ajuste URL

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = {
   e2e: {
-    baseUrl: 'https://agenda-contatos-react.vercel.app/',
+    baseUrl: 'https://ebac-agenda-contatos-tan.vercel.app/',
   },
 };
